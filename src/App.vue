3809f42791be6f6d4a7ddf76655b6ba2246c6436
Busra <i412469@student.fontys.nl>
2021-03-30 17:32:48 +0200

    Finished ToggleCard component

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,40 @@
 export default {
   name: 'App',
   components: {
-
+    
   }
 }
 </script>
 <style>
-
+@import url("https://use.typekit.net/cfk8vri.css");
+@import url("https://use.typekit.net/cfk8vri.css");
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: frank-new, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
+
+/*
+--Frank New Regular--
+font-family: frank-new, sans-serif;
+font-weight: 400;
+font-style: normal;
+
+--Frank New Bold--
+font-family: frank-new, sans-serif;
+font-weight: 700;
+font-style: normal;
+
+--Open Sans Bold--
+font-family: open-sans, sans-serif;
+font-weight: 700;
+font-style: normal;
+
+--Open Sans Regular--
+font-family: open-sans, sans-serif;
+font-weight: 400;
+font-style: normal;
+*/
 </style>
