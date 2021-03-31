6255ef31c45c46abd1fd72320e84619bc981a24e
Noah <noah.vanschijndel@live.nl>
2021-03-31 10:19:51 +0200

    Added overlay

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,49 @@
 <template>
   <router-view>
-
   </router-view>
+  <SlideOutOverlay title="Printer 1" />
 </template>
 
 <script>
+import SlideOutOverlay from "./components/Global/SlideOutOverlay";
 
 export default {
   name: 'App',
   components: {
-
+    SlideOutOverlay
   }
 }
 </script>
 <style>
-
+@import url("https://use.typekit.net/cfk8vri.css");
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: open-sans, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+/*
+--Frank New Regular--
+font-family: frank-new, sans-serif;
+font-weight: 400;
+font-style: normal;
+
+--Frank New Bold--
+font-family: frank-new, sans-serif;
+font-weight: 700;
+font-style: normal;
+
+--Open Sans Bold--
+font-family: open-sans, sans-serif;
+font-weight: 700;
+font-style: normal;
+
+--Open Sans Regular--
+font-family: open-sans, sans-serif;
+font-weight: 400;
+font-style: normal;
+*/
 </style>
