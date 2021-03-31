2e1fd60aeac738292984e0f7e8f2735b8412f0a0
Niek van de Vondervoort <416145@student.fontys.nl>
2021-03-31 15:06:34 +0200

    Basic date selector

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,29 @@
 <template>
   <h1>Home</h1>
   <a href="/machines">Go to machines</a>
-  <ToggleCard />
+  <div class="pt-15">
+    <ToggleCard />
+  </div>
+  <div class="pt-15">
+    <DatePicker id="datepicker" />
+  </div>
 </template>
 
 <script>
-
 import ToggleCard from "../components/Global/ToggleCard.vue"
+import DatePicker from "../components/Global/DatePicker.vue"
 
 export default {
   name: 'Home',
   components:{
     ToggleCard,
+    DatePicker
   } 
 }
 </script>
+
+<style>
+.pt-15 {
+  padding-top: 15px;
+}
+</style>
