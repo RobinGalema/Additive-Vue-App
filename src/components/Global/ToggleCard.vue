6e84e40a635d1dddb70ccb3582e3930e558325f9
Busra <i412469@student.fontys.nl>
2021-04-01 13:24:50 +0200

    Updated ToggleCard component to allow nested components

--- src/components/Global/ToggleCard.vue
+++ src/components/Global/ToggleCard.vue
@@ -1,22 +1,8 @@
 <template>
-    <!-- Component: <ToggleCard cardTitle="Title" /> ------->
-    <div class="cardBackground">
-        <div class="topRow"> <!-- als je op de topRow klikt veranderdt show naar het tegenovergestelde waarde -->
+        <div @click="show = !show, onClick()" class="topRow"> <!-- als je op de topRow klikt veranderdt show naar het tegenovergestelde waarde -->
             <h4>{{cardTitle}}</h4>
             <img :class="[show ? 'spinArrow' : '']" src="../../assets/icons/chevron-down.svg"> <!-- als "show" true is dan krijgt de div de "spinArrow" class, zo niet, dan krijgt die geen class.-->  
         </div>
-
-        <transition
-            @beforeEnter="beforeEnter"
-            @afterEnter="afterEnter"
-            @beforeLeave="beforeLeave"
-            @afterLeave="afterLeave">
-        <div v-show="show" class="cardContent">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        </div>
-        </transition>
-    </div>
-  
 </template>
 
 
@@ -34,25 +20,16 @@ export default {
         cardTitle: String
     },
     methods:{
-        
+        onClick(){
+            console.log("geklikt!!!")
+            this.$emit("show-card-content")
+        },
     },
 }
 
 </script>
 
 <style scoped>
-
-    .cardBackground{
-        background: #F8F8F8;
-        box-shadow: 0 0 10pt #00000029;
-        border-radius: 10px;
-        text-align: left;
-        padding-top: 2px;
-        padding-bottom: 2px;
-        width: 100%;
-        padding-left: 8px;
-        padding-right: 8px;
-    }
 
     .topRow{
         display: flex;
@@ -79,6 +56,56 @@ export default {
         transition: 0.4s; /*-- dit zorgt voor de animatie wanneer de arrow terugdraait */
         color: #151539;
     }
-
-
 </style>
+
+
+
+
+<!----- Plaats dit in de file waar je de component wil gebruiken 
+
+<template>
+  <h1>Home</h1>
+  <a href="/machines">Go to machines</a>
+  <div class="cardContainer">
+    <ToggleCard @show-card-content="showCardContent" cardTitle="Title" />
+    <div v-if="showTheContent">
+                  teeeest
+    </div>
+  </div>
+</template>
+<script>
+import ToggleCard from "@/components/Global/ToggleCard.vue"
+
+export default {
+  name: 'Home',
+  data(){
+    return{
+      showTheContent: false
+    }
+  },
+  components:{
+   ToggleCard,
+  },
+  methods:{
+    showCardContent(){
+      this.showTheContent = !this.showTheContent
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .cardContainer{
+        background: #F8F8F8;
+        box-shadow: 0 0 10pt #00000029;
+        border-radius: 10px;
+        text-align: left;
+        padding-top: 2px;
+        padding-bottom: 8px;
+        padding-left: 8px;
+        padding-right: 8px;
+        margin: 4px;
+    }
+</style>
+
+------>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <h1>Home</h1>
   <a href="/machines">Go to machines</a>
-  
+ 
 </template>
 <script>
-
 
 export default {
   name: 'Home',
   data(){
-  
+    return{
+    }
   },
   components:{
-   
   },
+  methods:{
+  
+  }
 }
 </script>
+
