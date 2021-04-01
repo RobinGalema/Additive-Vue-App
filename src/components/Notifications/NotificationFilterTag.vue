04be43f037bf1bd122d669a626ff5357a70a16e0
NickGraus <nick-graus@hotmail.com>
2021-04-01 15:04:52 +0200

    created NotificationFilterTag

---
+++ src/components/Notifications/NotificationFilterTag.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="notificationFilterTag">
+        <div class="filterTag">
+            Machine 1
+        </div>
+        <div class="cancelBtn">
+            <img src="@/assets/icons/cancel.png" />
+        </div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'NotificationFilterTag',
+        props: {
+            title: String,
+            context: String,
+            timestamp: String,
+            linkToMachine: String,
+            level: String
+        }
+    }
+</script>
+
+
+<style scoped>
+    .notificationFilterTag {
+        height: 20px;
+        background: #FCC517;
+        border-radius: 20px;
+        padding: 4px 10px;
+        font-size: 10pt;
+        color: #ffffff;
+        display: flex;
+    }
+
+    .filterTag {
+        width: 90%;
+    }
+
+    .cancelBtn {
+        width: 10%;
+        margin: 4px 0;
+    }
+
+    .cancelBtn img {
+        height: 100%;
+        float: right;
+    }
+</style>
