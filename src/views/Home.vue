40b176ec8f9bfa66439d7a8d830468f0eba94756
Busra <i412469@student.fontys.nl>
2021-04-01 18:10:53 +0200

    Added a new style class to GlobalButton >> .HomeScreenButton

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,71 @@
 <template>
-  <h1>Home</h1>
-  <a href="/machines">Go to machines</a>
- 
-</template>
-<script>
+  <div class="topSection">
+    <h1>Overview</h1>
+    <img src="../assets/icons/settings.svg">
+  </div>
+  <h4>{{date}}</h4>
 
-export default {
-  name: 'Home',
-  data(){
-    return{
-    }
-  },
-  components:{
-  },
-  methods:{
+  <div class="firstRow">
+    <HomeScreenDiagram />
+    <PlanNewJobButton text="Plan new job" version="homeScreenButton"  picture_src="../assets/logo.png"/>
+  </div>
   
+
+</template>
+
+<script>
+  import HomeScreenDiagram from "@/components/Homescreen/HomeScreenDiagram.vue"
+  import PlanNewJobButton from "@/components/Global/GlobalButton.vue"
+  export default {
+    name: 'Home',
+    data(){
+      return{
+        date: "",
+      }
+    },
+    components:{
+      HomeScreenDiagram,
+      PlanNewJobButton
+    },
+    methods:{
+      getCurrentDate: function () {
+          var currentDate = new Date();
+          const day = currentDate.toLocaleString('en', { weekday: 'short' });
+          const month = currentDate.toLocaleString('en', { month: 'long' });
+          const fullDate = day + ". " + month + " " + currentDate.getDate();
+          this.date = fullDate;
+        }
+    },
+    mounted() { //wordt uitgevoerd onload
+        this.getCurrentDate()
+    }
   }
-}
+
 </script>
+
 <style>
-.pt-15 {
-  padding-top: 15px;
+
+.topSection{
+  display: flex;
+  justify-content: space-between;
 }
+
+h1{
+  margin: 0px;
+  color: #000000;
+}
+
+h4{
+  margin-top: 0px;
+  margin-left: 4px;
+  text-transform: uppercase;
+  color: #BBBBBB;
+}
+
+.firstRow{
+  display: grid;
+  grid-template-columns: 70% 30%;
+}
+
+
 </style>
