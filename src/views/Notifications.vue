2c693396609530e84b2640f7907c7c171c7b46c5
NickGraus <nick-graus@hotmail.com>
2021-04-08 15:53:20 +0200

    mulitple errors and machines added

--- src/views/Notifications.vue
+++ src/views/Notifications.vue
@@ -5,16 +5,33 @@
         <NotificationFilterTags />
     </div>
 
-    <Dropdown dropdownTitle="Machine 1" dropdownContent="hallo"/>
+    <Dropdown dropdownTitle="Machine 1" dropdownContent=""/>
 
-    <NotificationCard title="Machine stopped" context="Machine 1 stopped because a fatal error occurred" minutes="20" linkToMachine="" level="high"/>
-    <NotificationCard title="Machine stopped" context="Machine 1 stopped because a fatal error occurred" minutes="25" linkToMachine="" level="high"/>
-    <NotificationCard title="Machine stopped" context="Machine 1 stopped because a fatal error occurred" minutes="36" linkToMachine="" level="high"/>
+    <NotificationCard title="Machine 1 stopped" context="Machine 1 stopped because a fatal error occurred" minutes="20" linkToMachine="" level="high"/>
+    <NotificationCard title="Low powder level" context="Machine 1's powder supply is almost empty" minutes="25" linkToMachine="" level="medium"/>
+    <NotificationCard title="Powder supply halfway empty" context="The powder basin of Machine 1 halfway empty" minutes="200" linkToMachine="" level="low"/>
 
-    <Dropdown dropdownTitle="Machine 4" dropdownContent="hallo"/>
+    <Dropdown dropdownTitle="Machine 2" dropdownContent=""/>
 
-    <NotificationCard title="Machine stopped" context="Machine 4 stopped because a fatal error occurred" minutes="48" linkToMachine="" level="high"/>
+    <NotificationCard title="Low powder level" context="Machine 2's powder supply is almost empty" minutes="48" linkToMachine="" level="medium"/>
+
+    <Dropdown dropdownTitle="Machine 3" dropdownContent=""/>
+
+    <NotificationCard title="Error pre-manufacturing" context="The print cannot be started because of a pre-manufactering error" minutes="48" linkToMachine="" level="high"/>
     <NotificationCard title="Machine stopped" context="Machine 4 stopped because a fatal error occurred" minutes="114" linkToMachine="" level="high"/>
+
+    <Dropdown dropdownTitle="Machine 4" dropdownContent=""/>
+
+    <Dropdown dropdownTitle="Machine 5" dropdownContent=""/>
+
+    <NotificationCard title="Low oxygen level" context="Machine 5's oxygen level is unstable" minutes="21" linkToMachine="" level="medium"/>
+    <NotificationCard title="Cannot start print due to a fatal error" context="No plate was found at machine 5" minutes="48" linkToMachine="" level="high"/>
+
+    <Dropdown dropdownTitle="Machine 6" dropdownContent=""/>
+
+    <Dropdown dropdownTitle="Machine 7" dropdownContent=""/>
+
+    <Dropdown dropdownTitle="Machine 8" dropdownContent=""/>
 
 
 </template>
